79832ac35556618eee4edb1d8d6e76e1d8e038bf
R0b1n-0510 <r0b1nofficial@gmail.com>
2023-04-05 16:35:23 +0530

    maked items removable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,36 @@
+import { useContext } from 'react'
+import CartItem from './CartItem'
 import Modal from '../UI/Modal'
 import styles from './Cart.module.css'
+import CartContext from '../../store/cart-context'
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext)
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0
+
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id)
+    }
+
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({...item, amount: 1})
+    }
+
     const cartItems = <ul className={styles['cart-items']}>
-        {[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 }].map(item => <li>{item.name}</li>)}
+        {cartCtx.items.map(item => <CartItem Key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}/>)}
     </ul>
 
     return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={styles.total}>
             <span>Total Amount</span>
-            <span>32.62</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={styles.actions}>
             <button className={styles['button--alt']} onClick = {props.onClose}>Close</button>
-            <button className={styles.button}>Order</button>
+            {hasItems && <button className={styles.button}>Order</button>}
         </div>
     </Modal>
 }
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from "react"
+import CartContext from "../../../store/cart-context"
 import styles from "./MealItem.module.css"
 import MealItemForm from "./MealItemForm"
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext)
+
     const price = `$${props.price.toFixed(2)}` // to render up to 2 decimal places
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return <li className={styles.meal}>
         <div><h3>{props.name}</h3>
@@ -10,7 +23,7 @@ const MealItem = (props) => {
             <div className={styles.price}>{price}</div>
         </div>
         <div>
-            <MealItemForm />
+            <MealItemForm onAddToCart={addToCartHandler}/>
         </div>
     </li>
 }
